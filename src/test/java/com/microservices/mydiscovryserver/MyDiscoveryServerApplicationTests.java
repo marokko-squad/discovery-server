b00f72bd025b663a30b48ddd1c1b83065270d866
JavaZakariae <bouali_zakariae@hotmail.fr>
2022-03-28 10:47:01 +0200

    initializing the discovery server

---
+++ src/test/java/com/microservices/mydiscovryserver/MyDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.mydiscovryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
